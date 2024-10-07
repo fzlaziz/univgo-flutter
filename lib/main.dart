6cb601cb6fd220d7703f13d77ee2b264d0eda3d3
Faizal Aziz <84029684+fzlaziz@users.noreply.github.com>
2024-10-08 05:48:16 +0700

    feat: implement bottom nav bar for main navigation
    - create custom app bar for profile page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:univ_go/presentation/univ_go_icon_icons.dart';
 
+const blueTheme = 0xff0059ff;
+const greyTheme = 0xff808080;
+
 void main() {
   runApp(MyApp());
 }
@@ -24,11 +27,49 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   final FocusNode _focusNode = FocusNode();
   final TextEditingController _searchController = TextEditingController();
+  int _selectedIndex = 0;
+  bool _isSearchMode = false;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _isSearchMode = false;
+    });
+    if (index == 1) {
+      setState(() {
+        _isSearchMode = true;
+      });
+      Navigator.of(context)
+          .push(
+        MaterialPageRoute(
+          builder: (context) => SearchPage(
+            searchController: _searchController,
+            focusNode: _focusNode,
+          ),
+        ),
+      )
+          .then((_) {
+        setState(() {
+          _selectedIndex = 0;
+          _searchController.clear();
+          _isSearchMode = false;
+        });
+        _focusNode.unfocus();
+      });
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    _focusNode.requestFocus();
+
+    _focusNode.addListener(() {
+      if (!_focusNode.hasFocus) {
+        setState(() {
+          _isSearchMode = false;
+        });
+      }
+    });
   }
 
   @override
@@ -45,59 +86,121 @@ class _MainPageState extends State<MainPage> {
         preferredSize: const Size.fromHeight(70.0),
         child: AppBar(
           toolbarHeight: 70,
-          backgroundColor: const Color(0xff0059ff),
-          title: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white, 
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  children: [
-                    const Icon(UnivGoIcon.search, color: Colors.black),
-                    const SizedBox(width: 15),
-                    Expanded(
-                      child: TextField(
-                        controller: _searchController,
-                        focusNode: _focusNode,
-                        decoration: InputDecoration(
-                          hintText: "Cari Perguruan Tinggi",
-                          hintStyle: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w400,
-                            color:
-                                Colors.black, 
-                          ),
-                          border: InputBorder.none,
-                        ),
-                        onTap: () {
-                          Navigator.of(context)
-                              .push(
-                            MaterialPageRoute(
-                              builder: (context) => SearchPage(
-                                searchController: _searchController,
-                                focusNode: _focusNode,
+          backgroundColor: const Color(blueTheme),
+          centerTitle: true,
+          title: _selectedIndex == 2
+              ? Center(
+                  child: Text(
+                    "Profile",
+                    style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xffffffff)),
+                  ),
+                )
+              : Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors
+                          .white, 
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        children: [
+                          const Icon(UnivGoIcon.search, color: Colors.black),
+                          const SizedBox(width: 15),
+                          Expanded(
+                            child: TextField(
+                              controller: _searchController,
+                              focusNode: _focusNode,
+                              decoration: InputDecoration(
+                                hintText: "Cari Perguruan Tinggi",
+                                hintStyle: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black,
+                                ),
+                                border: InputBorder.none,
                               ),
+                              onTap: () {
+                                setState(() {
+                                  _isSearchMode = true;
+                                });
+                                Navigator.of(context)
+                                    .push(
+                                  MaterialPageRoute(
+                                    builder: (context) => SearchPage(
+                                      searchController: _searchController,
+                                      focusNode: _focusNode,
+                                    ),
+                                  ),
+                                )
+                                    .then((_) {
+                                  setState(() {
+                                    _searchController.clear();
+                                    _isSearchMode = false;
+                                  });
+                                  _focusNode.unfocus();
+                                });
+                              },
                             ),
-                          )
-                              .then((_) {
-                            _searchController.clear();
-                          });
-                        },
+                          ),
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ),
           elevation: 0,
         ),
       ),
-      body: const Center(
-        child: Text('Main Page Content'),
+      bottomNavigationBar: _isSearchMode
+          ? null
+          : Container(
+              height: 65,
+              child: BottomNavigationBar(
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      UnivGoIcon.homeoutline,
+                    ),
+                    activeIcon: Icon(UnivGoIcon.home),
+                    label: 'Beranda',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(UnivGoIcon.search),
+                    label: 'Cari',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.person_outline,
+                      size: 30,
+                    ),
+                    activeIcon: Icon(
+                      Icons.person,
+                      size: 30,
+                    ),
+                    label: 'Profile',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                selectedItemColor: const Color(blueTheme),
+                unselectedItemColor: const Color(greyTheme),
+                onTap: _onItemTapped,
+                selectedLabelStyle: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 13,
+                ),
+                iconSize: 25,
+                unselectedLabelStyle: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 13,
+                ),
+              ),
+            ),
+      body: Center(
+        child: Text('Main Page Content with selected : $_selectedIndex'),
       ),
     );
   }
@@ -117,28 +220,32 @@ class SearchPage extends StatelessWidget {
         child: AppBar(
           automaticallyImplyLeading: false,
           toolbarHeight: 70,
-          backgroundColor: const Color(0xff0059ff),
+          backgroundColor: const Color(blueTheme),
           title: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 3),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.white, 
+                color: Colors.white,
                 borderRadius: BorderRadius.circular(10),
               ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Row(
                   children: [
-                    const Icon(Icons.search, color: Colors.black),
+                    const Icon(UnivGoIcon.search, color: Colors.black),
                     const SizedBox(width: 10),
                     Expanded(
                       child: TextField(
                         controller: searchController,
                         focusNode: focusNode,
                         decoration: const InputDecoration(
-                          hintText: "", 
+                          hintText: "",
                           hintStyle: TextStyle(color: Colors.black),
                           border: InputBorder.none,
+                        ),
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
                         ),
                         autofocus: true,
                       ),
