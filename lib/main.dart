3e4b93026a3e3ead819e8d8f8a4a93c43dfb303f
Faizal Aziz <84029684+fzlaziz@users.noreply.github.com>
2024-10-08 05:41:48 +0700

    feat: create app bar with search box

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,158 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:univ_go/presentation/univ_go_icon_icons.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Univ Go',
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final FocusNode _focusNode = FocusNode();
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70.0),
+        child: AppBar(
+          toolbarHeight: 70,
+          backgroundColor: const Color(0xff0059ff),
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white, 
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    const Icon(UnivGoIcon.search, color: Colors.black),
+                    const SizedBox(width: 15),
+                    Expanded(
+                      child: TextField(
+                        controller: _searchController,
+                        focusNode: _focusNode,
+                        decoration: InputDecoration(
+                          hintText: "Cari Perguruan Tinggi",
+                          hintStyle: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w400,
+                            color:
+                                Colors.black, 
+                          ),
+                          border: InputBorder.none,
+                        ),
+                        onTap: () {
+                          Navigator.of(context)
+                              .push(
+                            MaterialPageRoute(
+                              builder: (context) => SearchPage(
+                                searchController: _searchController,
+                                focusNode: _focusNode,
+                              ),
+                            ),
+                          )
+                              .then((_) {
+                            _searchController.clear();
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          elevation: 0,
         ),
+      ),
+      body: const Center(
+        child: Text('Main Page Content'),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  final TextEditingController searchController;
+  final FocusNode focusNode;
+
+  const SearchPage({required this.searchController, required this.focusNode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70.0),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          toolbarHeight: 70,
+          backgroundColor: const Color(0xff0059ff),
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 3),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white, 
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    const Icon(Icons.search, color: Colors.black),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        focusNode: focusNode,
+                        decoration: const InputDecoration(
+                          hintText: "", 
+                          hintStyle: TextStyle(color: Colors.black),
+                          border: InputBorder.none,
+                        ),
+                        autofocus: true,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          elevation: 0,
+        ),
+      ),
+      body: const Center(
+        child: Text('Search Results Page Content'),
       ),
     );
   }
