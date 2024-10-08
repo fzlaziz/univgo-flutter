677cb59d51a89043cb01042ccc9f4733b7c7dc87
Faizal Aziz <84029684+fzlaziz@users.noreply.github.com>
2024-10-08 13:14:09 +0700

    feat: separate SearchPage class into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:univ_go/presentation/univ_go_icon_icons.dart';
+import 'search_page.dart';
 
 const blueTheme = 0xff0059ff;
 const greyTheme = 0xff808080;
@@ -102,8 +103,7 @@ class _MainPageState extends State<MainPage> {
                   padding: const EdgeInsets.symmetric(horizontal: 0),
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Colors
-                          .white, 
+                      color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                     ),
                     child: Padding(
@@ -199,68 +199,17 @@ class _MainPageState extends State<MainPage> {
                 ),
               ),
             ),
-      body: Center(
-        child: Text('Main Page Content with selected : $_selectedIndex'),
-      ),
-    );
-  }
-}
-
-class SearchPage extends StatelessWidget {
-  final TextEditingController searchController;
-  final FocusNode focusNode;
-
-  const SearchPage({required this.searchController, required this.focusNode});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(70.0),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          toolbarHeight: 70,
-          backgroundColor: const Color(blueTheme),
-          title: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 3),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  children: [
-                    const Icon(UnivGoIcon.search, color: Colors.black),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: TextField(
-                        controller: searchController,
-                        focusNode: focusNode,
-                        decoration: const InputDecoration(
-                          hintText: "",
-                          hintStyle: TextStyle(color: Colors.black),
-                          border: InputBorder.none,
-                        ),
-                        style: GoogleFonts.poppins(
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
-                        autofocus: true,
-                      ),
-                    ),
-                  ],
+      body: _selectedIndex == 0
+          ? const Center(
+              child: Text('Beranda'),
+            )
+          : _selectedIndex == 1
+              ? const Center(
+                  child: Text('Cari'),
+                )
+              : const Center(
+                  child: Text('Profile'),
                 ),
-              ),
-            ),
-          ),
-          elevation: 0,
-        ),
-      ),
-      body: const Center(
-        child: Text('Search Results Page Content'),
-      ),
     );
   }
 }
---
+++ lib/search_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:univ_go/presentation/univ_go_icon_icons.dart';
+
+const blueTheme = 0xff0059ff;
+const greyTheme = 0xff808080;
+
+class SearchPage extends StatelessWidget {
+  final TextEditingController searchController;
+  final FocusNode focusNode;
+
+  const SearchPage({required this.searchController, required this.focusNode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70.0),
+        child: AppBar(
+          automaticallyImplyLeading: true,
+          iconTheme: const IconThemeData(color: Colors.white),
+          toolbarHeight: 70,
+          backgroundColor: const Color(blueTheme),
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 3),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    const Icon(UnivGoIcon.search, color: Colors.black),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        focusNode: focusNode,
+                        decoration: const InputDecoration(
+                          hintText: "",
+                          hintStyle: TextStyle(color: Colors.black),
+                          border: InputBorder.none,
+                        ),
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                        autofocus: true,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          elevation: 0,
+        ),
+      ),
+      body: const Center(
+        child: Text('Search Results Page Content'),
+      ),
+    );
+  }
+}
